aee7ce9762c675d7d7bfdeca0754df43f2917890
Outkarm <jkpordje@gmail.com>
2025-05-08 00:18:18 +0000

    add hero info component and styling

---
+++ src/components/HeroPageComponent/components/MyHeroInfo.jsx
@@ -0,0 +1,19 @@
+import "../styles/MyHeroInfo.css";
+const MyHeroInfo = () => {
+  return (
+    <div className="my-hero-cont">
+      <h1 className="my-hero-head-greeting">
+        Hey There. <br />
+        I'm John.
+      </h1>
+      <div className="my-hero-into">I’m a Software Developer</div>
+      <div className="my-hero-into-info">
+        I can help you build a product , feature or website. Look through some
+        of my work and experience! If you like what you see and have a project
+        you need coded, don’t hesitate to contact me.
+      </div>
+    </div>
+  );
+};
+
+export default MyHeroInfo;
