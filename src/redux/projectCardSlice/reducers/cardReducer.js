4e2641e0c1467a9875ab006b343f0bfcbfd07c02
Outkarm <jkpordje@gmail.com>
2025-06-21 23:58:17 +0000

    update card reducers to handle  new actions

--- src/redux/projectCardSlice/reducers/cardReducer.js
+++ src/redux/projectCardSlice/reducers/cardReducer.js
@@ -1,39 +1,45 @@
-import { CREATE_CARD } from "../actions/cardActions";
+import {
+  CREATE_CARD,
+  UPDATE_ALL_CARDS,
+  DELETE_CARD,
+  EDIT_CARD,
+} from "../actions/cardActions";
 
-//initial state
+// Initial state
 const initialState = {
-  projectName: "",
-  summary: "",
-  projectInfo: "",
-  projectBadges: [],
-  image: "",
-  sourceLink: "",
-  projectLink: "",
+  ProjectCards: [],
 };
 
 function cardReducer(state = initialState, action) {
-  if (action.type === CREATE_CARD) {
-    const {
-      projectName,
-      summary,
-      projectInfo,
-      projectBadges,
-      image,
-      sourceLink,
-      projectLink,
-    } = action.payload;
-    return {
-      ...state,
-      projectName: projectName,
-      summary: summary,
-      projectInfo: projectInfo,
-      projectBadges: projectBadges,
-      image: image,
-      sourceLink: sourceLink,
-      projectLink: projectLink,
-    };
-  } else {
-    return state;
+  switch (action.type) {
+    case CREATE_CARD:
+      return {
+        ...state,
+        ProjectCards: [...state.ProjectCards, action.payload],
+      };
+
+    case UPDATE_ALL_CARDS:
+      return {
+        ...state,
+        ProjectCards: action.payload,
+      };
+
+    case DELETE_CARD:
+      return {
+        ...state,
+        ProjectCards: state.ProjectCards.filter((_, i) => i !== action.payload),
+      };
+
+    case EDIT_CARD:
+      const updatedCards = [...state.ProjectCards];
+      updatedCards[action.payload.index] = action.payload.updatedCard;
+      return {
+        ...state,
+        ProjectCards: updatedCards,
+      };
+
+    default:
+      return state;
   }
 }
 
