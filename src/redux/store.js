77031b19cafccdb531193faff52a57bd5c998922
Outkarm <jkpordje@gmail.com>
2025-06-21 23:53:24 +0000

    fix persist config error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import cardReducer from "./projectCardSlice/reducers/cardReducer";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-// Configuration for Redux Persist
+// Persist config
 const persistConfig = {
-  key: "root", // Key under which the persisted state will be stored in local storage
-  storage, // Storage engine (e.g., localStorage)
-  //   blacklist: []
+  key: "root",
+  storage,
 };
 
 const rootReducer = combineReducers({ card: cardReducer });
-
-// Create a persisted reducer using Redux Persist
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Configure the Redux store with the persisted reducer
-const store = configureStore({ reducer: { portfolio: persistedReducer } });
+const store = configureStore({
+  reducer: { portfolio: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-// Create a persistor to manage persist/restore
 const persistor = persistStore(store);
 
-// Export the configured store and persistor for use in your app
 export { store, persistor };
