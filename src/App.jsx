d97d42bfe2fa154ea6e449f2bf2732931359cd2c
Outkarm <jkpordje@gmail.com>
2025-06-22 00:16:51 +0000

    add and configure rout for my recent works page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import HeroPage from "./pages/HeroPage.jsx";
+import MyRecentWorksPage from "./pages/MyRecentWorksPage.jsx";
 import NavBar from "./components/NavBarComponent/NavBar.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <HeroPage />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" Component={HeroPage} />
+          <Route path="/portfolio" Component={MyRecentWorksPage} />
+          <Route path="/contact" Component={MyRecentWorksPage} />
+          <Route path="/about" Component={MyRecentWorksPage} />
+        </Routes>
+      </Router>
     </div>
   );
 }
