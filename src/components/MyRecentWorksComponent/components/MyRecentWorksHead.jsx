d4e528d85c66ef853fa2ca305eae9948cb87e27d
Outkarm <jkpordje@gmail.com>
2025-06-23 14:13:42 +0000

    add header component dor MY RECENT WORKS page

---
+++ src/components/MyRecentWorksComponent/components/MyRecentWorksHead.jsx
@@ -0,0 +1,12 @@
+import "../styles/MyRecentWorksHead.css";
+
+const MyRecentWorksHead = () => {
+  return (
+    <div className="my-recent-works-page-head-container">
+      <div className="my-recent-works-page-head">My Recent Works</div>
+      <div className="horizontal-line"></div>
+    </div>
+  );
+};
+
+export default MyRecentWorksHead;
