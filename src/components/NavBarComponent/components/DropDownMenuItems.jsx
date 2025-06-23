fb6a4580f56749dcaaf1f18196044d69b4c68971
Outkarm <jkpordje@gmail.com>
2025-06-23 15:57:38 +0000

    update mobile dropdown list items to link to pages asigned

--- src/components/NavBarComponent/components/DropDownMenuItems.jsx
+++ src/components/NavBarComponent/components/DropDownMenuItems.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import "../style/DropDownMenuItems.css";
 
 const DropDownMenuItems = ({ dropDownMenuItems }) => {
   return (
     <>
       {dropDownMenuItems.map((item, index) => (
-        <div className="dd-menu-item" key={index}>
-          {item}
-        </div>
+        <Link to={`/${item.toLowerCase()}`} key={index}>
+          <div className="dd-menu-item" key={index}>
+            {item}
+          </div>
+        </Link>
       ))}
     </>
   );
