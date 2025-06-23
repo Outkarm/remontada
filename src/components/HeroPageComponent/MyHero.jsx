927ec45800e440d592ed03ed64c72b7e6971169a
Outkarm <jkpordje@gmail.com>
2025-06-23 14:03:25 +0000

    add function to load cards  whe hero oage loads

--- src/components/HeroPageComponent/MyHero.jsx
+++ src/components/HeroPageComponent/MyHero.jsx
@@ -1,7 +1,39 @@
 import "./styles/MyHero.css";
 import MyHeroInfo from "./components/MyHeroInfo";
 import MyHeroSocials from "./components/MyHeroSocials";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { useEffect } from "react";
+import { updateAllCards } from "../../redux/projectCardSlice/actions/cardActions";
+
 const MyHero = () => {
+  const dispatch = useDispatch();
+  const loadCards = async () => {
+    try {
+      const response = await axios
+        .get("http://127.0.0.1:3000/projects")
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          return data;
+        })
+        .catch((error) => {
+          console.error(`Error Lpading Cards From Database: ${error}`);
+        });
+      console.log("Project loaded:", response);
+      dispatch(updateAllCards(response));
+    } catch (error) {
+      console.error(`Error Lpading Cards From Database: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    const handleLoadCards = async () => {
+      await loadCards();
+    };
+    handleLoadCards();
+  }, []);
+
   return (
     <div className="my-hero-container">
       <div className="my-hero-info-container">
