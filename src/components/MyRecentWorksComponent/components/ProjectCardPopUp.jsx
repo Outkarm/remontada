1c899f2a42ebd4c84d7231a530636b26972d948f
Outkarm <jkpordje@gmail.com>
2025-06-23 15:43:15 +0000

    add project pop up card component

---
+++ src/components/MyRecentWorksComponent/components/ProjectCardPopUp.jsx
@@ -0,0 +1,73 @@
+import "../styles/ProjectCardPopUp.css";
+import Budge from "../../GeneralAccessoriesComponent/Budge";
+import Button from "../../GeneralAccessoriesComponent/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+const ProjectCardPopUp = ({
+  imgURL,
+  bdgList = [],
+  projectName,
+  projectInfo,
+  seeLiveLink,
+  seeSourceLink,
+  setOpenPopUpCard,
+}) => {
+  const handleCloseBTN = () => {
+    setOpenPopUpCard(false);
+  };
+
+  const handleSeeProjectLive = () => {
+    window.open(seeLiveLink);
+  };
+
+  const handleSeeProjectSource = () => {
+    window.open(seeSourceLink);
+  };
+
+  return (
+    <div className="pop-up-card-container">
+      <div className="pop-up-card-sub-container-1">
+        <div className="pop-up-project-name-and-badge-container">
+          <div className="pop-up-project-name">{projectName}</div>
+          <div className="pop-up-badge-list-container">
+            {bdgList.map((bdgName, index) => (
+              <Budge bdgText={bdgName} bdgID={index} bdgType={"pop-up"} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <button onClick={handleCloseBTN} className="close-btn">
+            <FontAwesomeIcon size="3x" icon={faXmark} />
+          </button>
+        </div>
+      </div>
+      <div className="pop-up-card-sub-container-2">
+        <div className="pop-up-card-image-container">
+          <img className="project-image" src={imgURL} alt={projectName} />
+        </div>
+        <div className="pop-up-card-info-and-btn-container">
+          <div className="project-info">{projectInfo}</div>
+          <div className="pop-up-buttons-container">
+            <Button
+              btnText={"See Live"}
+              btnType="pop-up"
+              costomBlock={true}
+              clickFunction={handleSeeProjectLive}
+              isLiveLink={true}
+            />
+            <Button
+              btnText={"See Source"}
+              btnType="pop-up"
+              costomBlock={true}
+              clickFunction={handleSeeProjectSource}
+              isSourceLink={true}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCardPopUp;
