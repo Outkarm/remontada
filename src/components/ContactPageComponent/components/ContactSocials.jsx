495f88e34e514661e6c3351b585f7aec022e980b
Outkarm <jkpordje@gmail.com>
2025-07-01 14:25:26 +0000

    add contact quote component

---
+++ src/components/ContactPageComponent/components/ContactSocials.jsx
@@ -0,0 +1,15 @@
+import "../styles/ContactSocials.css";
+
+const ContactSocials = () => {
+  return (
+    <div className="contact-socials-container">
+      <img src="/images/contactImg/github.svg" alt="github" />
+      <img src="/images/contactImg/linkedIn.svg" alt="linkedIn" />
+      <img src="/images/contactImg/angelList.svg" alt="angelList" />
+      <img src="/images/contactImg/twitter.svg" alt="twitter" />
+      <img src="/images/contactImg/medium.svg" alt="medium" />
+    </div>
+  );
+};
+
+export default ContactSocials;
