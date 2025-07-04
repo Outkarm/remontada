e847e0e38974dee5ce2a562415e9e3820ff87cd5
Outkarm <jkpordje@gmail.com>
2025-07-04 15:53:54 +0000

    track button loading and add dounload function for pdf download

--- src/components/AboutPagecomponent/components/AboutInfoComponent.jsx
+++ src/components/AboutPagecomponent/components/AboutInfoComponent.jsx
@@ -1,7 +1,35 @@
-import "../styles/AboutInfoComponent.css";
+import { useState } from "react";
 import Button from "../../GeneralAccessoriesComponent/Button.jsx";
+import "../styles/AboutInfoComponent.css";
 
 const AboutInfoComponent = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleDownload = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch("/pdf/John_Kpordje_Resume.pdf");
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "John_Kpordje_Resume.pdf";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Download failed", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // Downloads\Documents\video_5.pdf
+
   return (
     <div className="about-info-container">
       <div className="about-info-header">About me</div>
@@ -10,7 +38,13 @@ const AboutInfoComponent = () => {
         through some of my work and experience! If you like what you see and
         have a project you need coded, don’t hesitate to contact me.
       </div>
-      <Button btnText={"Get my resume"} btnUse={"pop-up"} costomBlock={true} />
+      <Button
+        btnText={"Get my resume"}
+        clickFunction={handleDownload}
+        btnUse={"pop-up"}
+        costomBlock={true}
+        isLoading={isLoading}
+      />
     </div>
   );
 };
