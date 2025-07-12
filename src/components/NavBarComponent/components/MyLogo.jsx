9952b555bc06735295971e30f2555a67af38bb73
Outkarm <jkpordje@gmail.com>
2025-07-12 16:48:11 +0000

    add logo image

--- src/components/NavBarComponent/components/MyLogo.jsx
+++ src/components/NavBarComponent/components/MyLogo.jsx
@@ -1,10 +1,33 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "../style/MyLogo.css";
-const MyLogo = () => {
+const MyLogo = ({ setIsLogoClicked, isLogoClicked }) => {
+  const toggleClick = () => setIsLogoClicked(!isLogoClicked);
+  const adminAccess = useSelector((state) => state.portfolio.admin.adminAccess);
+
   return (
-    <Link to={"/"}>
-      <div className="my-logo">Koy Outkarm</div>
-    </Link>
+    <div className="logo-container">
+      <div
+        className="logo-image-container"
+        style={
+          adminAccess === "live"
+            ? {
+                border: "5px solid green",
+              }
+            : { border: "5px solid rgba(255, 0, 0, 0.511)" }
+        }
+        onClick={toggleClick}
+      >
+        {adminAccess === "live" ? (
+          <img src="/logo/logo3.jpg" alt="My Logo" className="logo-image" />
+        ) : (
+          <img src="/logo/logo-1.png" alt="My Logo" className="logo-image" />
+        )}
+      </div>
+      <Link to={"/"}>
+        <div className="my-logo">Koy Outkarm</div>
+      </Link>
+    </div>
   );
 };
 export default MyLogo;
